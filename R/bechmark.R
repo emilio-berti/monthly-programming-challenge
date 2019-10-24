8a6f8fdaff1c233a03d1df650481daeb833e9721
Emilio Berti <emilio.berti@bios.au.dk>
2019-10-24 13:34:04 +0200

    benchmarking script added

---
+++ R/bechmark.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+library(rbenchmark)
+library(wesanderson)
+
+source('../monkey_themes.R')
+
+scripts <- list.files(pattern = '.txt')
+participants <- sub('[.]txt', '', scripts)
+
+bench <- list()
+for(name in participants){
+  i <- which(participants == name)
+  bench[[i]] <- sapply(1:50, function(x) benchmark(source(scripts[i]), replications = 50)$elapsed)
+}
+
+names(bench) <- participants
+
+res <- bench %>% 
+  bind_cols() %>% 
+  gather()
+
+res %>% 
+  ggplot() +
+  geom_violin(aes(reorder(key, - value), value, fill = key)) +
+  geom_jitter(aes(reorder(key, - value), value), width = 0.1) +
+  xlab('') +
+  ylab('Execution time') +
+  scale_fill_manual(values = wes_palette('Darjeeling1', 10, type = 'continuous')) +
+  coord_flip() +
+  monkey_bare
